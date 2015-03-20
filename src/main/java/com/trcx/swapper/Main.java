224520f93b82c56f60e3f0381f0cbd278517f765
Trcx <trcx528@gmail.com>
2015-03-19 22:02:27 -0500

    1.0.4 jar build

--- src/main/java/com/trcx/swapper/Main.java
+++ src/main/java/com/trcx/swapper/Main.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 @Mod(modid = "swapper", version = Main.VERSION, name = "Swapper")
 public class Main
 {
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     public static Item Swapper;
     public static String[] swapperBlacklist = new String[2];
